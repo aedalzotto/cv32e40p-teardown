9be40b7df60a4fd6af0b236024f2f684b1c62313
Angelo Elias Dalzotto <angelo.dalzotto@edu.pucrs.br>
2022-04-13 17:43:49 -0300

    Simplify TEST makefile

--- cobj_dir/Vtb_top_verilator__ConstPool_0.d
+++
@@ -1,6 +0,0 @@
-Vtb_top_verilator__ConstPool_0.o: Vtb_top_verilator__ConstPool_0.cpp \
- /home/angelo.dalzotto/.local/verilator/include/verilated.h \
- /home/angelo.dalzotto/.local/verilator/include/verilatedos.h \
- /home/angelo.dalzotto/.local/verilator/include/verilated_config.h \
- /home/angelo.dalzotto/.local/verilator/include/verilated_types.h \
- /home/angelo.dalzotto/.local/verilator/include/verilated_funcs.h
--- cobj_dir/Vtb_top_verilator__Dpi.d
+++
@@ -1,9 +0,0 @@
-Vtb_top_verilator__Dpi.o: Vtb_top_verilator__Dpi.cpp \
- Vtb_top_verilator__Dpi.h \
- /home/angelo.dalzotto/.local/verilator/include/vltstd/svdpi.h \
- Vtb_top_verilator.h \
- /home/angelo.dalzotto/.local/verilator/include/verilated.h \
- /home/angelo.dalzotto/.local/verilator/include/verilatedos.h \
- /home/angelo.dalzotto/.local/verilator/include/verilated_config.h \
- /home/angelo.dalzotto/.local/verilator/include/verilated_types.h \
- /home/angelo.dalzotto/.local/verilator/include/verilated_funcs.h
